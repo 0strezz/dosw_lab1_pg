2014b5944bea02417495fde2f675c2d8462664a6
0strezz <juan.dpmz05@gmail.com>
2026-02-04 00:00:41 -0500

    reto 3 merged

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,17 +1,20 @@
 import java.util.stream.IntStream;
+import java.util.function.Function;
 
 public class Reto3 {
+
+    public static void main(String[] args) {
+        Function<String, String> procesar = mensaje -> transformarMensaje(mensaje);
+        System.out.println(procesar.apply("Hola Mundo"));
+    }
+
     public static String transformarMensaje(String mensaje) {
         StringBuilder builder = new StringBuilder();
         IntStream.range(0, 3)
-                .forEach(i -> builder.append(mensaje).append(" "));
-        return builder.toString().trim();
-    }
-}
+                 .forEach(i -> builder.append(mensaje).append(" "));
+        String repetido = builder.toString().trim();
 
-public class Reto3 {
-    public static String transformarMensaje(String mensaje) {
-        StringBuffer buffer = new StringBuffer(mensaje);
+        StringBuffer buffer = new StringBuffer(repetido);
         return buffer.reverse().toString();
     }
 }
